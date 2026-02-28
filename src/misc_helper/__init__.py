5f2d62d1c13a7a8b0a21f0facbab40429100eff1
SakuraKooi <me@sakurakooi.dev>
2026-02-28 21:57:04 +0800

    feat: prompt preprocessor

--- src/misc_helper/__init__.py
+++ src/misc_helper/__init__.py
@@ -1,3 +1,4 @@
+from misc_helper.nodes.prompt_preprocessor import PromptPreprocessor
 from misc_helper.nodes.sampler_config import KSamplerAdvancedConfig, KSamplerConfigExtract
 from misc_helper.nodes.aspect_ratio import AspectRatioCalculator
 from misc_helper.utils.utils import mapNodeClass
@@ -7,10 +8,12 @@ NODE_CLASS_MAPPINGS = {
     mapNodeClass(AspectRatioCalculator.NAME): AspectRatioCalculator,
     mapNodeClass(KSamplerAdvancedConfig.NAME): KSamplerAdvancedConfig,
     mapNodeClass(KSamplerConfigExtract.NAME): KSamplerConfigExtract,
+    mapNodeClass(PromptPreprocessor.NAME): PromptPreprocessor,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     mapNodeClass(AspectRatioCalculator.NAME): AspectRatioCalculator.NAME,
     mapNodeClass(KSamplerAdvancedConfig.NAME): KSamplerAdvancedConfig.NAME,
     mapNodeClass(KSamplerConfigExtract.NAME): KSamplerConfigExtract.NAME,
+    mapNodeClass(PromptPreprocessor.NAME): PromptPreprocessor.NAME,
 }
---
+++ src/misc_helper/nodes/prompt_preprocessor.py
@@ -0,0 +1,34 @@
+
+from inspect import cleandoc
+
+from misc_helper.utils.utils import getNodeCategory
+
+
+class PromptPreprocessor:
+    """ Cleanup prompt text, remove extra spaces, newlines, etc. """
+
+    def __init__(self):
+        pass
+
+    NAME = "Prompt Preprocessor"
+    CATEGORY = getNodeCategory("prompt")
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "prompt_text": ("STRING", ),
+            },
+        }
+
+    RETURN_TYPES = ("STRING")
+    RETURN_NAMES = ("prompt_text")
+    DESCRIPTION = cleandoc(__doc__)
+    FUNCTION = "handle"
+
+    def handle(self, prompt_text):
+        prompt_text = prompt_text.strip()
+        prompt_text = prompt_text.replace('\n', ' ').replace('\r', ' ')
+        prompt_text = prompt_text.replace(',', ', ')
+        prompt_text = ' '.join(prompt_text.split())
+        return (prompt_text)
