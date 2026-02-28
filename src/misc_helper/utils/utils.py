9a08b99490aa6d6ad5f379c9ccc43c2e4cdba70c
SakuraKooi <me@sakurakooi.dev>
2026-02-28 22:37:32 +0800

    refactor: add error details

--- src/misc_helper/utils/utils.py
+++ src/misc_helper/utils/utils.py
@@ -8,4 +8,4 @@ def getNodeCategory(category):
 
 def validateDim(v: int):
     if v <= 0 or v % 8 != 0:
-        raise ValueError("dimension must > 0 and divisible by 8")
+        raise ValueError("dimension must > 0 and divisible by 8, got: {}".format(v))
